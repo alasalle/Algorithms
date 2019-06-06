a2c55998986908be38261814515e9f7e183be3da
alasalle <angela.lasalle91@gmail.com>
2019-06-06 00:53:15 -0400

    completed stock_prices
    completed recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 
 import math
+import operator
+
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches = {}
+    for item, amount in recipe.items():
+        if item not in ingredients:
+            return 0
+        batches[item] = ingredients[item] // amount
+    return min(batches.items(), key=operator.itemgetter(1))[1]
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,32 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    print(prices)
+    current_min_price = 0
+    max_profit_so_far = 0
+    prices = [int(p) for i, p in enumerate(prices)]
+    for i, p in enumerate(prices):
+        if i == 0:
+            current_min_price = p
+        elif i == 1:
+            max_profit_so_far = p - current_min_price
+            if p < current_min_price:
+                current_min_price = p
+        else:
+            calcul = p - current_min_price
+            if calcul > max_profit_so_far:
+                max_profit_so_far = calcul
+            if p < current_min_price:
+                current_min_price = p
+    return max_profit_so_far
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
